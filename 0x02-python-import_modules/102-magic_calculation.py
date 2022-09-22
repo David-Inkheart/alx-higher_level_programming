9f8c360b6c6a67d5fda36cc02d22dd86dbe9591c
David-Inkheart <okoliedave1@gmail.com>
2022-09-22 16:31:42 -0700

    Python bytecode 3

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Tip: Read documentation on Python bytecode"""
+def magic_calculation(a, b):
+    """Match bytecode provided by Alx-Holberton School."""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return sub(a, b)
