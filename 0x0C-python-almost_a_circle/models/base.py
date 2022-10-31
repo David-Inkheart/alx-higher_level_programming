3af52feb6515e826fcc5f84af6edf8b397f3c067
David-Inkheart <okoliedave1@gmail.com>
2022-10-31 13:34:02 -0700

    returns the list of the JSON string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,7 +4,8 @@ This class will be the “base” of all other classes in this project
 The goal of it is to manage id attribute in all future classes
 and to avoid duplicating the same code (by extension, same bugs)
 '''
-
+import json
+import csv
 
 class Base:
     '''My base class'''
