9df95c7787e2430983abe42944c9274f83ba8e47
David-Inkheart <okoliedave1@gmail.com>
2022-11-02 03:20:03 -0700

    rebel class that inherits from Int but behaves opposite to it

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,3 +2,21 @@
 '''
 a class MyInt that inherits from int:
 '''
+
+
+class MyInt(int):
+    '''MyInt is a rebel.
+    MyInt has == and != operators inverted
+    '''
+
+    def __eq__(self, value):
+        '''Magid method equals
+        '''
+
+        return super().__ne__(value)
+
+    def __ne__(self, value):
+        '''Magic method not equals
+        '''
+
+        return super().__eq__(value)
