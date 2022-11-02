880479d6c5513d1c5da8b8bffe4fa52e32d87458
David-Inkheart <okoliedave1@gmail.com>
2022-11-02 02:14:01 -0700

    area() and str() implemented

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
-'''
- a class Rectangle that inherits BaseGeometry.
-'''
+"""
+Building a class - Rectangle inherits from BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    child class using parent class function
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return '[{}] {}/{}'.format(self.__class__.__name__,
+                                   self.__width, self.__height)
