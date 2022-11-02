646638f5797c25454dc704903d66d8edaf6bf701
David-Inkheart <okoliedave1@gmail.com>
2022-11-02 02:43:38 -0700

    implement ares() and str()

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
-'''
-a class Square that inherits from Rectangle (9-rectangle.py
-'''
+"""
+Building a class - Square inherits from Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    grandchild class
+    """
+    def __init__(self, size):
+        self.__size = size
+
+        super().integer_validator('size', size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return '[{}] {}/{}'.format(self.__class__.__name__,
+                                   self.__size, self.__size)
