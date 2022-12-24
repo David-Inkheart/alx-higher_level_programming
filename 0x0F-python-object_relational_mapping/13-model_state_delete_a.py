bab8a8440dffd56ae9c2f46d9b536768204051f1
David-Inkheart <okoliedave1@gmail.com>
2022-12-24 03:23:43 -0800

    update a database with SQLAlchemy

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script changes the name 
-of a State object
+This script deletes all state objects
+with a name containing 'a'
 from the database `hbtn_0e_6_usa`.
 """
 
@@ -18,11 +18,12 @@ if __name__ == "__main__":
     db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
     engine = create_engine(db)
+
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    new_state = session.query(State).filter(State.id == '2').first()
-    new_state.name = 'New Mexico'
+    for instance in session.query(State).filter(State.name.contains('a')):
+         session.delete(instance)
     session.commit()
     session.close()
