28cb2f32183155209686439ed88cf6de7104f9e2
David-Inkheart <okoliedave1@gmail.com>
2023-01-16 13:39:59 -0800

    size of the response body in bytes

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displaying the size of the response body in bytes
+# This program takes in a URL, sends a request to that URL, and displays the size of the body of the response.
 curl -sI "$1" | awk '/Content-Length/ {print $2}'
