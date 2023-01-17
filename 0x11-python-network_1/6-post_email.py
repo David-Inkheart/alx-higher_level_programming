d9cff496ab3705a7bc250bc6df00d1f814026129
David-Inkheart <okoliedave1@gmail.com>
2023-01-17 15:55:30 -0800

    fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,10 +5,10 @@ to the passed URL with the email as a parameter, and finally displays
 the body of the response
 """
 
-from requests import post
+import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    r = post(argv[1], data={'email': argv[2]}):
+    r = requests.post(argv[1], data={'email': argv[2]}):
         print(r.text)
