f4005ced7cae65328b3163f20d87b1330f2045c3
David-inkheart <David-Inkheart@github.com>
2023-02-09 20:33:55 +0100

    reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// read a file, store its contents in a variable
+// then log its contents to the console
+
+const fs = require('fs').promises;
+const process = require('process');
+
+async function readFile (filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    console.log(data.toString());
+  } catch (error) {
+    console.error(`${error}`);
+  }
+}
+
+readFile(process.argv[2]);
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,11 @@
+# Tasks on JavaScript - Web scraping
+
+## 0. Readme
+
+### Write a script that reads and prints the content of a file.
+
+_ The first argument is the file path
+
+_ The content of the file must be read in utf-8
+
+_ If an error occurred during the reading, print the error object
