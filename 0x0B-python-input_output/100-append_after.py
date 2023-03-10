d66df96366809bd2ee9fffdf26577cba2eaf9f0a
David-Inkheart <okoliedave1@gmail.com>
2023-03-10 08:51:36 +0100

    fixed code to pep8 standard

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -2,6 +2,7 @@
 """inserts text in a file
 """
 
+
 def append_after(filename="", search_string="", new_string=""):
     """Insert text after each line containing a given string in a file.
     Args:
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
+
+
 def pascal_triangle(n):
     if n <= 0:
         return []
